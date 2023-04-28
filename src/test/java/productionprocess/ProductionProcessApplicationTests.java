4b0a46f4846f4fa9b8f2814cc5e7b51bfef7b6e5
Lisa-1919 <lizasimerova438@gmail.com>
2023-04-29 00:15:35 +0300

    front_end

---
+++ src/test/java/productionprocess/ProductionProcessApplicationTests.java
@@ -0,0 +1,13 @@
+package productionprocess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductionProcessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
