223363e88385ddde5bfd5f0e38b4cded43bf74bd
WhyMChibisova <chibisova3586@gmail.com>
2023-05-05 20:47:42 +0300

    add enum StatusOrderAtWarehouse, edit status, method to get orders by workshop

--- src/main/java/productionprocess/services/OrderOnProductionService.java
+++ src/main/java/productionprocess/services/OrderOnProductionService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import productionprocess.data.entities.*;
 import productionprocess.data.model.MaterialToOrder;
+import productionprocess.data.model.StatusOrderOnProduction;
 import productionprocess.data.repo.MaterialRepo;
 import productionprocess.data.repo.OrderOnProductionDetailsRepo;
 import productionprocess.data.repo.OrderOnProductionRepo;
@@ -40,12 +41,19 @@ public class OrderOnProductionService {
         orderOnProductionRepo.save(orderOnProductionDB);
     }
 
+    public void editStatus(int id, StatusOrderOnProduction statusOrderOnProduction) {
+        OrderOnProduction orderOnProductionDB = orderOnProductionRepo.findById(id).orElseThrow();
+        String status = statusOrderOnProduction.getStatus();
+        orderOnProductionDB.setStatus(status);
+        orderOnProductionRepo.save(orderOnProductionDB);
+    }
+
     public boolean deleteOrderOnProduction(int id) {
-        try{
+        try {
             OrderOnProduction orderOnProduction = orderOnProductionRepo.findById(id).orElseThrow();
             orderOnProductionRepo.delete(orderOnProduction);
             return true;
-        } catch (NoSuchElementException e){
+        } catch (NoSuchElementException e) {
             return false;
         }
 
@@ -70,7 +78,7 @@ public class OrderOnProductionService {
         return result;
     }
 
-    public List<MaterialToOrder> getNecessaryQuantity(int orderId){
+    public List<MaterialToOrder> getNecessaryQuantity(int orderId) {
         OrderOnProduction orderOnProduction = findById(orderId);
         List<OrderOnProductionDetails> orderOnProductionDetails = orderOnProduction.getOrderOnProductionDetails();
         List<MaterialToOrder> materialToOrders = new ArrayList<>();
@@ -78,11 +86,11 @@ public class OrderOnProductionService {
         for (OrderOnProductionDetails orderOnProductionDetail : orderOnProductionDetails) {
             Product product = orderOnProductionDetail.getProduct();
             double amount = orderOnProductionDetail.getAmount();
-            for (MaterialsForProduct materialsForProduct : product.getMaterialsForProducts()){
-                if (materialNameAndNecessaryQuantity.containsKey(materialsForProduct.getMaterial().getName())){
+            for (MaterialsForProduct materialsForProduct : product.getMaterialsForProducts()) {
+                if (materialNameAndNecessaryQuantity.containsKey(materialsForProduct.getMaterial().getName())) {
                     materialNameAndNecessaryQuantity.put(materialsForProduct.getMaterial().getName(),
-                             materialNameAndNecessaryQuantity.get(materialsForProduct.getMaterial().getName())
-                                     + materialsForProduct.getQuantity() * amount);
+                            materialNameAndNecessaryQuantity.get(materialsForProduct.getMaterial().getName())
+                                    + materialsForProduct.getQuantity() * amount);
                 } else {
                     materialNameAndNecessaryQuantity.put(materialsForProduct.getMaterial().getName(),
                             materialsForProduct.getQuantity() * amount);
@@ -96,11 +104,54 @@ public class OrderOnProductionService {
             double stockQuantity = material.getStockQuantity();
             double necessaryQuantity = map.getValue();
             double orderedQuantity = necessaryQuantity - stockQuantity;
-            if(orderedQuantity < 0) {
+            if (orderedQuantity < 0) {
                 orderedQuantity = 0;
             }
             materialToOrders.add(new MaterialToOrder(name, stockQuantity, necessaryQuantity, orderedQuantity));
         }
         return materialToOrders;
+    }
+
+    public List<OrderOnProduction> getOrderByWorkShop(String workshop) {
+        List<OrderOnProduction> orderOnProductions = orderOnProductionRepo.findAll();
+        List<OrderOnProduction> result = new ArrayList<>();
+        switch (workshop) {
+            case "Сборочный цех": {
+                for (OrderOnProduction orderOnProduction : orderOnProductions) {
+                    if (orderOnProduction.getStatus().equals(StatusOrderOnProduction.STATUS_1.getStatus()) ||
+                            orderOnProduction.getStatus().equals(StatusOrderOnProduction.STATUS_2.getStatus()) ||
+                            orderOnProduction.getStatus().equals(StatusOrderOnProduction.STATUS_3.getStatus()) ||
+                            orderOnProduction.getStatus().equals(StatusOrderOnProduction.STATUS_4.getStatus()) ||
+                            orderOnProduction.getStatus().equals(StatusOrderOnProduction.STATUS_5.getStatus()) ||
+                            orderOnProduction.getStatus().equals(StatusOrderOnProduction.STATUS_6.getStatus())) {
+                        result.add(orderOnProduction);
+                    }
+                }
+                break;
+            }
+            case "Покрасочный цех": {
+                for (OrderOnProduction orderOnProduction : orderOnProductions) {
+                    if (orderOnProduction.getStatus().equals(StatusOrderOnProduction.STATUS_6.getStatus()) ||
+                            orderOnProduction.getStatus().equals(StatusOrderOnProduction.STATUS_7.getStatus()) ||
+                            orderOnProduction.getStatus().equals(StatusOrderOnProduction.STATUS_8.getStatus()) ||
+                            orderOnProduction.getStatus().equals(StatusOrderOnProduction.STATUS_9.getStatus())) {
+                        result.add(orderOnProduction);
+                    }
+                }
+                break;
+            }
+            case "Упаковочный цех": {
+                for (OrderOnProduction orderOnProduction : orderOnProductions) {
+                    if (orderOnProduction.getStatus().equals(StatusOrderOnProduction.STATUS_9.getStatus()) ||
+                            orderOnProduction.getStatus().equals(StatusOrderOnProduction.STATUS_10.getStatus()) ||
+                            orderOnProduction.getStatus().equals(StatusOrderOnProduction.STATUS_11.getStatus()) ||
+                            orderOnProduction.getStatus().equals(StatusOrderOnProduction.STATUS_12.getStatus())) {
+                        result.add(orderOnProduction);
+                    }
+                }
+                break;
+            }
+        }
+        return result;
     }
 }
